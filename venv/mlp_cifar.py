12efcc2cb696fcdd484ae2c6566fd6542a3d6c2d
varshitha3035 <varshithakatakam3035@gmail.com>
2025-07-24 09:58:56 +0530

    final version

--- venv/mlp_cifar.py
+++ venv/mlp_cifar.py
@@ -1,4 +1,4 @@
-import libraries
+#import libraries
 from keras.models import Sequential
 from keras.layers import Dense,Flatten
 from keras.datasets import cifar10
@@ -15,3 +15,27 @@ y_test=to_categorical(y_test)
 #buid the architecture
 model=Sequential()
 model.add(Flatten(input_shape=(32,32,3)))
+model.add(Dense(1024,activation='relu'))
+model.add(Dense(512,activation='relu'))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(10,activation='softmax'))
+
+#compile
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'],validation_split=0.2)
+
+#Train
+history=model.fit(X_train,y_train,epochs=10,batch_size=64)
+
+
+#Evaluate
+test_accuracy,loss=model.evaluate(X_test,y_test)
+print(f'test_accuracy:{test_accuracy}')
+print(f'loss:{loss}')
+
+#Visualization
+plt.plot(history.history['accuracy'],color='blue',label='train_accuracy')
+plt.plot(history.history['val_accuracy'],color='red',label='val_accuracy')
+plt.legend()
+plt.title('Epochs Vs Accuracy')
+plt.show()
